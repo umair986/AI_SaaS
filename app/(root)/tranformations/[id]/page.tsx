b197eb272a4044712dd84a4dbf86150c6d3af3f6
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-08-25 00:13:44 +0530

    clerk auth added

---
+++ app/(root)/tranformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransformationsPage = () => {
+  return <div></div>;
+};
+
+export default TransformationsPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
+import { ClerkProvider, SignIn } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-const inter = IBM_Plex_Sans({
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
-  title: "Cubo_Image_Editior",
-  description: "AI powered Image Generator",
+  title: "Cubo AI",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -19,8 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          <SignIn routing="hash" />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
